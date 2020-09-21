8af80034b96ecb6485ffd27ce904ffb6bdcf138c
andyb95 <andybeutler95@gmail.com>
2020-09-20 23:53:42 -0600

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+
+  return (
+    <div className='home'>
+      <div className='carousel'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Nav.css'
+
+const Nav = () => {
+
+  return (
+    <div className='nav'>
+      <div className='logo'>Cascade Networking</div>
+    </div>
+  )
+}
+
+export default Nav
